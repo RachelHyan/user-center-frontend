ba187ae1a345c14ac395f0a1850ef08fdfeaa1e0
heyan <heyan@citees.cn>
2023-03-27 10:54:04 +0800

    build: build

--- src/utils/globalRequest.ts
+++ src/utils/globalRequest.ts
@@ -12,7 +12,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://43.139.160.242:8080' : undefined,
+  prefix: process.env.NODE_ENV === 'production' ? 'http://43.139.160.242' : undefined,
   // requestType: 'form',
   timeout: 10000,
 });
